4c1813b98ed3402f7e83edbb0e12cbcd94332978
Baad C0de <baadc0de@pm.me>
2025-03-29 18:55:31 +0000

    Added json and python.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,8 +3,10 @@ return {
     opts = {
         formatters_by_ft = {
             javascript = { "prettierd", "prettier", stop_after_first = true },
+            json = { "prettierd", "prettier" },
             rust = { "rustfmt", lsp_format = "fallback" },
             lua = { "stylua" },
+            python = { "black" },
         },
         format_on_save = {
             timeout_ms = 500,
