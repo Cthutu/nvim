dbc248b7897308284fc359bef53646d929855e8a
Baad C0de <baadc0de@pm.me>
2025-03-29 18:20:18 +0000

    Reformatted some files.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,51 +7,51 @@ end
 
 new_opts = {
     -- Indentation control
-    expandtab = true,       -- Convert tabs to spaces
-    shiftwidth = 4,         -- Amount to ident with << and >>
-    tabstop = 4,            -- How many spaces are shown per tab
-    softtabstop = 4,        -- How many spaces are applied when pressing Tab
+    expandtab = true, -- Convert tabs to spaces
+    shiftwidth = 4, -- Amount to ident with << and >>
+    tabstop = 4, -- How many spaces are shown per tab
+    softtabstop = 4, -- How many spaces are applied when pressing Tab
     smarttab = true,
     smartindent = true,
-    autoindent = true,      -- Keep indentation from previous line
-    breakindent = true,     -- Wrapped lines will keep their indentation
+    autoindent = true, -- Keep indentation from previous line
+    breakindent = true, -- Wrapped lines will keep their indentation
 
     -- Line numbers
-    number = true,          -- Always show line numbers
-    relativenumber = true,  -- Show relative line numbers
-    cursorline = true,      -- Show line under cursor
+    number = true, -- Always show line numbers
+    relativenumber = true, -- Show relative line numbers
+    cursorline = true, -- Show line under cursor
     -- cursorcolumn = true,
 
     -- Undo
-    undofile = true,        -- Store undos between sessions
+    undofile = true, -- Store undos between sessions
 
     -- Mouse support
-    mouse = "a",            -- Enable mouse (for splits support)
+    mouse = "a", -- Enable mouse (for splits support)
 
     -- Don't show the mode (as we already use it mini.statusline)
     showmode = false,
 
     -- Case sensitivity
-    ignorecase = true,      -- By default, ignore case
-    smartcase = true,       -- Use case sensitivity if a caps is in the search term
+    ignorecase = true, -- By default, ignore case
+    smartcase = true, -- Use case sensitivity if a caps is in the search term
 
     -- Sign column always (can show warning flags etc).
     signcolumn = "yes",
 
     -- Splits control
-    splitright = true,      -- Vertical splits appear on the right
-    splitbelow = true,      -- Horizontal splits appear below
+    splitright = true, -- Vertical splits appear on the right
+    splitbelow = true, -- Horizontal splits appear below
 
     -- Whitespace visibility
     list = true,
     listchars = {
         tab = "» ",
         trail = "·",
-        nbsp = "˽"
+        nbsp = "˽",
     },
 
     -- Presentation control
-    scrolloff = 10,         -- Number of lines to keep above and below the cursor.
+    scrolloff = 10, -- Number of lines to keep above and below the cursor.
     --cmdheight = 0,          -- Disable command line, giving us an extra line.
 }
 
@@ -60,7 +60,7 @@ vim.opt = table_merge(vim.opt, new_opts)
 -- Highlight text for some time after yanking
 vim.api.nvim_create_autocmd("TextYankPost", {
     group = vim.api.nvim_create_augroup("YankHighlight", {
-        clear = true
+        clear = true,
     }),
     pattern = "*",
     callback = function()
@@ -70,4 +70,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 vim.g.have_nerd_font = true
-
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,12 +2,13 @@ return {
     "stevearc/conform.nvim",
     opts = {
         formatters_by_ft = {
-            rust = { "rustfmt", lsp_format = "fallback" }
+            javascript = { "prettierd", "prettier", stop_after_first = true },
+            rust = { "rustfmt", lsp_format = "fallback" },
+            lua = { "stylua" },
         },
         format_on_save = {
             timeout_ms = 500,
             lsp_format = "fallback",
-        }
-    }
+        },
+    },
 }
-
